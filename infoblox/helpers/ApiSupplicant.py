1cfce4568be13a4f830bc5f84fbd48bb8d99a0ca
Marco Buratto <m.buratto@lumit.it>
2022-03-03 10:48:02 +0100

    ApiSupplicant on a DRY diet

--- infoblox/helpers/ApiSupplicant.py
+++ infoblox/helpers/ApiSupplicant.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from base64 import b64encode
 import requests
 
@@ -19,7 +20,8 @@ class ApiSupplicant:
         self.silent = silent
 
         self.responseStatus = 500
-        self.responseObject = dict()
+        self.responsePayload = dict()
+        self.responseHeaders = dict()
 
 
 
@@ -28,7 +30,77 @@ class ApiSupplicant:
     ####################################################################################################################
 
     def get(self) -> dict:
-        # Fetches the resource from the HTTP REST API endpoint specified.
+        try:
+            Log.actionLog(
+                "[API Supplicant] Fetching remote: GET "+str(self.endpoint)+" with params: "+str(self.params)
+            )
+
+            return self.__request(requests.get, params=self.params)
+        except Exception as e:
+            raise e
+
+
+
+    def post(self, data: str, additionalHeaders: dict = None) -> dict:
+        additionalHeaders = {} if additionalHeaders is None else additionalHeaders
+
+        try:
+            Log.actionLog("[API Supplicant] Posting to remote: "+str(self.endpoint))
+            Log.actionLog("[API Supplicant] Posting data: "+str(data))
+
+            return self.__request(requests.post, additionalHeaders=additionalHeaders, data=data)
+        except Exception as e:
+            raise e
+
+
+
+    def put(self, data: str, additionalHeaders: dict = None) -> dict:
+        additionalHeaders = {} if additionalHeaders is None else additionalHeaders
+
+        try:
+            Log.actionLog(
+                "[API Supplicant] Putting to remote: "+str(self.endpoint)
+            )
+
+            return self.__request(requests.put, additionalHeaders=additionalHeaders, data=data)
+        except Exception as e:
+            raise e
+
+
+
+    def patch(self, data: str, additionalHeaders: dict = None) -> dict:
+        additionalHeaders = {} if additionalHeaders is None else additionalHeaders
+
+        try:
+            Log.actionLog(
+                "[API Supplicant] Patching remote: "+str(self.endpoint)
+            )
+
+            return self.__request(requests.patch, additionalHeaders=additionalHeaders, data=data)
+        except Exception as e:
+            raise e
+
+
+
+    def delete(self) -> dict:
+        try:
+            Log.actionLog(
+                "[API Supplicant] Deleting remote: "+str(self.endpoint)
+            )
+
+            return self.__request(requests.delete)
+        except Exception as e:
+            raise e
+
+
+
+    ####################################################################################################################
+    # Private methods
+    ####################################################################################################################
+
+    def __request(self, request: Callable, additionalHeaders: dict = None, params: dict = None, data: str = ""):
+        params = {} if params is None else params
+        additionalHeaders = {} if additionalHeaders is None else additionalHeaders
 
         # In the event of a network problem (e.g. DNS failure, refused connection, etc), Requests will raise a ConnectionError exception.
         # If a request times out, a Timeout exception is raised.
@@ -37,290 +109,47 @@ class ApiSupplicant:
 
         # On KO status codes, a CustomException is raised, with response status and body.
 
+        headers = {
+            "Authorization": self.authorization
+        }
+
+        headers.update(additionalHeaders)
+
         try:
-            # Fetch the remote resource from the Infoblox backend.
-            response = requests.get(self.endpoint,
+            response = request(self.endpoint,
                 proxies=self.httpProxy,
                 verify=self.tlsVerify,
                 timeout=settings.API_SUPPLICANT_NETWORK_TIMEOUT,
-
-                headers={
-                    "Authorization": self.authorization
-                },
-                params=self.params # GET parameters.
+                headers=headers,
+                params=params, # GET parameters.
+                data=data
             )
 
             self.responseStatus = response.status_code
+            self.responseHeaders = response.headers
 
             try:
-                self.responseObject = response.json()
+                self.responsePayload = response.json()
             except Exception:
-                self.responseObject = {}
-
-            Log.actionLog("Fetching remote: GET "+str(self.endpoint)+" with params: "+str(self.params)) # here for threaded calls.
-            Log.actionLog("Remote response HTTP status: "+str(self.responseStatus))
+                self.responsePayload = {}
 
             if not self.silent:
-                Log.actionLog("Remote response headers: "+str(response.headers))
-                Log.actionLog("Remote response payload: "+str(self.responseObject))
-            else:
-                Log.actionLog("Remote response headers: silenced by caller.")
-                Log.actionLog("Remote response payload: silenced by caller.")
+                for j in (("status", self.responseStatus), ("headers", self.responseHeaders), ("payload", self.responsePayload)):
+                    Log.actionLog("[API Supplicant] Remote response "+j[0]+": "+str(j[1]))
 
-            if self.responseStatus == 200: # ok.
+            # CustomException errors on connection ok but ko status code.
+            if self.responseStatus == 200 or self.responseStatus == 201: # ok / ok on POST.
                 pass
             elif self.responseStatus == 401:
                 raise CustomException(status=400, payload={"Infoblox": "Wrong credentials for the asset."})
             else:
-                if "text" in self.responseObject:
-                    infobloxError = self.responseObject["text"]
+                if "message" in self.responsePayload:
+                    err = self.responsePayload["message"]
                 else:
-                    infobloxError = self.responseObject
+                    err = self.responsePayload
 
-                raise CustomException(status=self.responseStatus, payload={"Infoblox": infobloxError})
-        except Exception as e:
-            raise e
-        return self.responseObject
-
-
-
-    def post(self, data: object, additionalHeaders: dict = {}) -> dict:
-        # In the event of a network problem (e.g. DNS failure, refused connection, etc), Requests will raise a ConnectionError exception.
-        # If a request times out, a Timeout exception is raised.
-        # If a request exceeds the configured number of maximum redirections, a TooManyRedirects exception is raised.
-        # SSLError on SSL/TLS error.
-
-        # On KO status codes, a CustomException is raised, with response status and body.
-
-        standardHeaders = {
-            "Authorization": self.authorization
-        }
-
-        # Merge headers.
-        for k, v in standardHeaders.items():
-            if standardHeaders[k] in additionalHeaders:
-                standardHeaders[k] = additionalHeaders[k] # additionalHeaders wins on common headers.
-
-        headers = {**standardHeaders, **additionalHeaders} # merge dicts.
-
-        try:
-            Log.actionLog("Posting to remote: "+str(self.endpoint))
-            Log.actionLog(data)
-
-            response = requests.post(self.endpoint,
-                proxies=self.httpProxy,
-                verify=self.tlsVerify,
-                timeout=settings.API_SUPPLICANT_NETWORK_TIMEOUT,
-
-                headers=headers,
-                params=None,
-                data=data
-            )
-
-            self.responseStatus = response.status_code
-
-            try:
-                self.responseObject = response.json()
-            except Exception:
-                self.responseObject = {}
-
-            Log.actionLog("Remote response status: "+str(self.responseStatus))
-            Log.actionLog("Remote response headers: "+str(response.headers))
-            Log.actionLog("Remote response payload: "+str(self.responseObject))
-
-            if self.responseStatus == 201 or self.responseStatus == 200: # 201 created + 200 created-for-dummies.
-                pass
-            elif self.responseStatus == 401:
-                raise CustomException(status=400, payload={"Infoblox": "Wrong credentials for the asset."})
-            else:
-                if "text" in self.responseObject:
-                    infobloxError = self.responseObject["text"]
-                else:
-                    infobloxError = self.responseObject
-
-                raise CustomException(status=self.responseStatus, payload={"Infoblox": infobloxError})
+                raise CustomException(status=self.responseStatus, payload={"Infoblox": err})
         except Exception as e:
             raise e
 
-        return self.responseObject
-
-
-
-    def put(self, data: object, additionalHeaders: dict = {}) -> dict:
-        # In the event of a network problem (e.g. DNS failure, refused connection, etc), Requests will raise a ConnectionError exception.
-        # If a request times out, a Timeout exception is raised.
-        # If a request exceeds the configured number of maximum redirections, a TooManyRedirects exception is raised.
-        # SSLError on SSL/TLS error.
-
-        # On KO status codes, a CustomException is raised, with response status and body.
-
-        standardHeaders = {
-            "Authorization": self.authorization
-        }
-
-        # Merge headers.
-        for k, v in standardHeaders.items():
-            if standardHeaders[k] in additionalHeaders:
-                standardHeaders[k] = additionalHeaders[k] # additionalHeaders wins on common headers.
-
-        headers = {**standardHeaders, **additionalHeaders} # merge dicts.
-
-        try:
-            Log.actionLog("Putting to remote: "+str(self.endpoint))
-
-            response = requests.put(self.endpoint,
-                proxies=self.httpProxy,
-                verify=self.tlsVerify,
-                timeout=settings.API_SUPPLICANT_NETWORK_TIMEOUT,
-
-                headers=headers,
-                params=None,
-                data=data
-            )
-
-            self.responseStatus = response.status_code
-
-            try:
-                self.responseObject = response.json()
-            except Exception:
-                self.responseObject = {}
-
-            Log.actionLog("Remote response status: "+str(self.responseStatus))
-            Log.actionLog("Remote response headers: "+str(response.headers))
-            Log.actionLog("Remote response payload: "+str(self.responseObject))
-
-            if self.responseStatus == 200: # ok.
-                pass
-            elif self.responseStatus == 401:
-                raise CustomException(status=400, payload={"Infoblox": "Wrong credentials for the asset."})
-            else:
-                if "text" in self.responseObject:
-                    infobloxError = self.responseObject["text"]
-                else:
-                    infobloxError = self.responseObject
-
-                raise CustomException(status=self.responseStatus, payload={"Infoblox": infobloxError})
-        except Exception as e:
-            raise e
-
-        return self.responseObject
-
-
-
-    def patch(self, data: object, additionalHeaders: dict = {}) -> dict:
-        # In the event of a network problem (e.g. DNS failure, refused connection, etc), Requests will raise a ConnectionError exception.
-        # If a request times out, a Timeout exception is raised.
-        # If a request exceeds the configured number of maximum redirections, a TooManyRedirects exception is raised.
-        # SSLError on SSL/TLS error.
-
-        # On KO status codes, a CustomException is raised, with response status and body.
-
-        standardHeaders = {
-            "Authorization": self.authorization
-        }
-
-        # Merge headers.
-        for k, v in standardHeaders.items():
-            if standardHeaders[k] in additionalHeaders:
-                standardHeaders[k] = additionalHeaders[k] # additionalHeaders wins on common headers.
-
-        headers = {**standardHeaders, **additionalHeaders} # merge dicts.
-
-        try:
-            Log.actionLog("Patching remote: "+str(self.endpoint))
-
-            response = requests.patch(self.endpoint,
-                proxies=self.httpProxy,
-                verify=self.tlsVerify,
-                timeout=settings.API_SUPPLICANT_NETWORK_TIMEOUT,
-
-                headers=headers,
-                params=None,
-                data=data
-            )
-
-            self.responseStatus = response.status_code
-
-            try:
-                self.responseObject = response.json()
-            except Exception:
-                self.responseObject = {}
-
-            Log.actionLog("Remote response status: "+str(self.responseStatus))
-            Log.actionLog("Remote response headers: "+str(response.headers))
-            Log.actionLog("Remote response payload: "+str(self.responseObject))
-
-            if self.responseStatus == 200: # ok.
-                pass
-            elif self.responseStatus == 401:
-                raise CustomException(status=400, payload={"Infoblox": "Wrong credentials for the asset."})
-            else:
-                if "text" in self.responseObject:
-                    infobloxError = self.responseObject["text"]
-                else:
-                    infobloxError = self.responseObject
-
-                raise CustomException(status=self.responseStatus, payload={"Infoblox": infobloxError})
-        except Exception as e:
-            raise e
-
-        return self.responseObject
-
-
-
-    def delete(self, additionalHeaders: dict = {}) -> dict:
-        # In the event of a network problem (e.g. DNS failure, refused connection, etc), Requests will raise a ConnectionError exception.
-        # If a request times out, a Timeout exception is raised.
-        # If a request exceeds the configured number of maximum redirections, a TooManyRedirects exception is raised.
-        # SSLError on SSL/TLS error.
-
-        # On KO status codes, a CustomException is raised, with response status and body.
-
-        standardHeaders = {
-            "Authorization": self.authorization
-        }
-
-        # Merge headers.
-        for k, v in standardHeaders.items():
-            if standardHeaders[k] in additionalHeaders:
-                standardHeaders[k] = additionalHeaders[k] # additionalHeaders wins on common headers.
-
-        headers = {**standardHeaders, **additionalHeaders} # merge dicts.
-
-        try:
-            Log.actionLog("Deleting remote: "+str(self.endpoint))
-
-            response = requests.delete(self.endpoint,
-                proxies=self.httpProxy,
-                verify=self.tlsVerify,
-                timeout=settings.API_SUPPLICANT_NETWORK_TIMEOUT,
-
-                headers=headers
-            )
-
-            self.responseStatus = response.status_code
-
-            try:
-                self.responseObject = response.json()
-            except Exception:
-                self.responseObject = {}
-
-            Log.actionLog("Remote response status: "+str(self.responseStatus))
-            Log.actionLog("Remote response headers: "+str(response.headers))
-            Log.actionLog("Remote response payload: "+str(self.responseObject))
-
-            if self.responseStatus == 200: # ok.
-                pass
-            elif self.responseStatus == 401:
-                raise CustomException(status=400, payload={"Infoblox": "Wrong credentials for the asset."})
-            else:
-                if "text" in self.responseObject:
-                    infobloxError = self.responseObject["text"]
-                else:
-                    infobloxError = self.responseObject
-
-                raise CustomException(status=self.responseStatus, payload={"Infoblox": infobloxError})
-        except Exception as e:
-            raise e
-
-        return self.responseObject
+        return self.responsePayload
