d2720b983b0eec9483356b99666a8be47180306d
Marco Buratto <m.buratto@lumit.it>
2022-03-08 10:05:12 +0100

    Rel 2.16

--- infoblox/usecases/impl/Ipv4Reserve.py
+++ infoblox/usecases/impl/Ipv4Reserve.py
@@ -5,5 +5,11 @@ from abc import ABCMeta, abstractmethod
 
 class Ipv4Reserve(metaclass=ABCMeta):
     @abstractmethod
+    def __init__(self, assetId: int, request: str, userData: dict, username: str, *args, **kwargs):
+        pass
+
+
+
+    @abstractmethod
     def reserve(self) -> Dict[str, List]:
         pass
