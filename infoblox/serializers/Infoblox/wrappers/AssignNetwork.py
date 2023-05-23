b21d6df982f5e2d73af0ed1755e485f3bee4add9
mik <m.sartori@lumit.it>
2023-05-23 16:51:29 +0200

    Assign cloud network: mandatory fields

--- infoblox/serializers/Infoblox/wrappers/AssignNetwork.py
+++ infoblox/serializers/Infoblox/wrappers/AssignNetwork.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
 
-from infoblox.serializers.Infoblox.Network import InfobloxNetworkSerializer
-
-
 class InfobloxAssignNetworkSerializer(serializers.Serializer):
+    class InfobloxAssignNetworkDataSerializer(serializers.Serializer):
+        class InfobloxNetworkValueStringSerializer(serializers.RegexField):
+            def __init__(self, *args, **kwargs):
+                regex = '^([01]?\d\d?|2[0-4]\d|25[0-5])(?:\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])){3}(?:/[0-2]\d|/3[0-2])?$|^next-available$'
+                super().__init__(regex=regex, *args, **kwargs)
+
+        class InfobloxNetworkInnerExtattrsSerializer(serializers.Serializer):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+
+                class InfobloxNetworkInnerExtattrsValueStringSerializer(serializers.Serializer):
+                    value = serializers.CharField(max_length=255)
+
+                self.fields["Reference"] = InfobloxNetworkInnerExtattrsValueStringSerializer(required=True)
+                self.fields["Account ID"] = InfobloxNetworkInnerExtattrsValueStringSerializer(required=True)
+                self.fields["Account Name"] = InfobloxNetworkInnerExtattrsValueStringSerializer(required=True)
+
+        network = InfobloxNetworkValueStringSerializer(required=True)
+        extattrs = InfobloxNetworkInnerExtattrsSerializer(required=True)
+        comment = serializers.CharField(max_length=255, allow_blank=True, required=True)
+
+
     provider = serializers.CharField(max_length=255, required=True)
     region = serializers.CharField(max_length=255, required=True)
-    network_data = InfobloxNetworkSerializer(required=True)
+    network_data = InfobloxAssignNetworkDataSerializer(required=True)
