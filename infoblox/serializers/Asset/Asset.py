2929f65ef4a0b1e1a4813d8809f80368da68bd42
mik <m.sartori@lumit.it>
2023-06-12 18:12:53 +0200

    Asset: non-mandatory fields

--- infoblox/serializers/Asset/Asset.py
+++ infoblox/serializers/Asset/Asset.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 
 
 class InfobloxAssetSerializer(serializers.Serializer):
-    address = serializers.CharField(max_length=64, required=True) # @todo: only valid data.
+    id = serializers.IntegerField(required=False)
+    address = serializers.CharField(max_length=64, required=False, allow_blank=True) # @todo: only valid data.
     fqdn = serializers.CharField(max_length=255, required=True, allow_blank=True) # @todo: only valid data.
     baseurl = serializers.CharField(max_length=255, required=True) # @todo: only valid data.
     tlsverify = serializers.IntegerField(required=True)
     datacenter = serializers.CharField(max_length=255, required=True, allow_blank=True)
     environment = serializers.CharField(max_length=255, required=True)
-    position = serializers.CharField(max_length=255, required=True, allow_blank=True)
-    username = serializers.CharField(max_length=64, required=True)
-    password = serializers.CharField(max_length=64, required=True)
+    position = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    username = serializers.CharField(max_length=64, required=False)
+    password = serializers.CharField(max_length=64, required=False)
--- infoblox/serializers/Asset/Assets.py
+++ infoblox/serializers/Asset/Assets.py
@@ -1,15 +1,6 @@
 from rest_framework import serializers
+from infoblox.serializers.Asset.Asset import InfobloxAssetSerializer
 
 
 class InfobloxAssetsSerializer(serializers.Serializer):
-    class InfobloxAssestItems(serializers.Serializer):
-        id = serializers.IntegerField(required=True) # @todo: only valid data.
-        address = serializers.CharField(max_length=64, required=False) # @todo: only valid data.
-        fqdn = serializers.CharField(max_length=255, required=True) # @todo: only valid data.
-        baseurl = serializers.CharField(max_length=255, required=True)
-        tlsverify = serializers.IntegerField(required=True)
-        datacenter = serializers.CharField(max_length=255, required=True)
-        environment = serializers.CharField(max_length=255, required=True)
-        position = serializers.CharField(max_length=255, required=False)
-
-    items = InfobloxAssestItems(many=True)
+    items = InfobloxAssetSerializer(many=True, required=False)
