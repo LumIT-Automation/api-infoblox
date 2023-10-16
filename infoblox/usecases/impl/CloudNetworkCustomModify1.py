58472ec0b94754ea95cad46474a82b76fce90203
mik <m.sartori@lumit.it>
2023-10-16 15:38:18 +0200

    Modify cloud network: do not break the triplet Account ID - Account Name - Reference

--- infoblox/usecases/impl/CloudNetworkCustomModify1.py
+++ infoblox/usecases/impl/CloudNetworkCustomModify1.py
@@ -68,14 +68,24 @@ class CloudNetworkCustomModify1(CloudNetworkModify):
                     if previousNetworks:
                         # Get the Account Name from the first occurrence.
                         accountName = previousNetworks[0].get("extattrs", {}).get("Account Name", {}).get("value", "")
-                        extattrs.update({"Account Name": {"value": accountName}})
+                        if accountName:
+                            extattrs.update({"Account Name": {"value": accountName}})
+                        # Get the Reference from the first occurrence.
+                        reference = previousNetworks[0].get("extattrs", {}).get("Reference", {}).get("value", "")
+                        if reference:
+                            extattrs.update({"Reference": {"value": reference}})
                 else:
                     accountName = extattrs.get("Account Name", {}).get("value", "")
                     if accountName:
                         previousNetworks = self.__getAccountNetworks(accountName=accountName)
                         # Get the Account ID from the first occurrence.
                         accountId = previousNetworks[0].get("extattrs", {}).get("Account ID", {}).get("value", "")
-                        extattrs.update({"Account ID": {"value": accountId}})
+                        if accountId:
+                            extattrs.update({"Account ID": {"value": accountId}})
+                        # Get the Reference from the first occurrence.
+                        reference = previousNetworks[0].get("extattrs", {}).get("Reference", {}).get("value", "")
+                        if reference:
+                            extattrs.update({"Reference": {"value": reference}})
 
             # Add existing extattrs fields of the network that are missing in the input data.
             for k, v in self.networkData["extattrs"].items():
