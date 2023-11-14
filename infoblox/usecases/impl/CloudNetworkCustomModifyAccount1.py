69f4cdc7095473e0a98a27e0d066da9ea7193c49
mik <m.sartori@lumit.it>
2023-11-14 12:02:59 +0100

    cloud network check fixes

--- infoblox/usecases/impl/CloudNetworkCustomModifyAccount1.py
+++ infoblox/usecases/impl/CloudNetworkCustomModifyAccount1.py
@@ -27,6 +27,9 @@ class CloudNetworkCustomModifyAccount1(CloudNetworkModifyAccount):
         l= list()
 
         try:
+            if not self.oldAccountIdNetworks:
+                raise CustomException(status=400, payload={"Infoblox": "No network found with this Account ID: " + self.accountId})
+
             self.__checkNetworkCountLimits(extrattrs=data, oldAccountIdNetworks=self.oldAccountIdNetworks)
 
             for net in self.oldAccountIdNetworks:
@@ -77,66 +80,71 @@ class CloudNetworkCustomModifyAccount1(CloudNetworkModifyAccount):
 
 
     def __checkNetworkCountLimits(self, extrattrs: dict, oldAccountIdNetworks: list):
-        totalNetworks = []
-
         try:
-            newAccountIdNetworks = self.__getAccountCloudNetworks(accountId=extrattrs.get("Account ID", {}).get("value", ""))
+            newAccountId = extrattrs.get("Account ID", {}).get("value", "")
+            newAccountName = extrattrs.get("Account Name", {}).get("value", "")
+            newReference = extrattrs.get("Reference", {}).get("value", "")
+            newAccountIdNetworks = self.__getAccountCloudNetworks(accountId=newAccountId)
 
-            if newAccountIdNetworks:
-                for net in newAccountIdNetworks: # check if the Account ID was already used with a different Account Name or Reference.
-                    if net.get("extattrs", {}).get("Account Name", {}).get("value", "") != extrattrs.get("Account Name", {}).get("value", ""):
-                        raise CustomException(
-                            status=400,
-                            payload={
-                                "Infoblox": "A network with the Account ID " + extrattrs.get("Account ID", {}).get("value", "") +
-                                            " but different Account Name exists: " + net["network"]
-                            }
-                        )
-                    if net.get("extattrs", {}).get("Reference", {}).get("value", "") != extrattrs.get("Reference", {}).get("value", ""):
-                        raise CustomException(
-                            status=400,
-                            payload={
-                                "Infoblox": "A network with the Account ID " + extrattrs.get("Account ID", {}).get("value", "") +
-                                            " but different Reference exists: " + net["network"]
-                            }
-                        )
-
-                totalNetworks.extend(newAccountIdNetworks) # the total number of networks for the changed account must respect the configured limits after the change.
-
-            newAccountNameNetworks = self.__getAccountCloudNetworks(accountName=extrattrs.get("Account Name", {}).get("value", ""))
-            if newAccountNameNetworks:
-                for net in newAccountNameNetworks: # also check if the Account Name was already used with a different Account ID.
-                    if net.get("extattrs", {}).get("Account ID", {}).get("value", "") != extrattrs.get("Account ID", {}).get("value", ""):
-                        raise CustomException(
-                            status=400,
-                            payload={
-                                "Infoblox": "A network with the Account Name " + extrattrs.get("Account Name", {}).get("value", "") +
-                                            " but different Account ID exists: " + net["network"]
-                            }
-                        )
-
-            if totalNetworks:
-                totalNetworks.extend(oldAccountIdNetworks)
-
-                # CLOUD_MAX_ACCOUNT_REGION is the maximum number of regions for Account ID.
-                if hasattr(settings, "CLOUD_MAX_ACCOUNT_REGION"):
-                    if settings.CLOUD_MAX_ACCOUNT_REGION < len(set([net.get("extattrs", {}).get("City", {}).get("value", "") for net in totalNetworks])):
-                        raise CustomException(
-                            status=400,
-                            payload={
-                                "Infoblox": "Cannot change the account: others networks exists for this account and the maximum number of regions for account should not be exceed: " + str(settings.CLOUD_MAX_ACCOUNT_REGION)
-                            }
-                        )
-
-                # CLOUD_MAX_ACCOUNT_REGION_NETS is the maximum number of networks for Account ID in a region.
-                if hasattr(settings, "CLOUD_MAX_ACCOUNT_REGION_NETS"):
-                    regionsNetworks = [ net.get("extattrs", {}).get("City", {}).get("value", "") for net in totalNetworks ]
-                    for region in set(regionsNetworks):
-                        if settings.CLOUD_MAX_ACCOUNT_REGION_NETS < regionsNetworks.count(region):
+            if newAccountId != self.accountId:
+                if newAccountIdNetworks:
+                    for net in newAccountIdNetworks: # check if the Account ID was already used with a different Account Name or Reference.
+                        if net.get("extattrs", {}).get("Account Name", {}).get("value", "") != newAccountName:
                             raise CustomException(
                                 status=400,
                                 payload={
-                                    "Infoblox": "Cannot change the account: others networks exists for this account and the maximum number of networks for region for account should not be exceed: " + str(settings.CLOUD_MAX_ACCOUNT_REGION_NETS)}
+                                    "Infoblox": "A network with the Account ID " + newAccountId +
+                                                " but different Account Name exists: " + net["network"]
+                                }
+                            )
+                        if net.get("extattrs", {}).get("Reference", {}).get("value", "") != newReference:
+                            raise CustomException(
+                                status=400,
+                                payload={
+                                    "Infoblox": "A network with the Account ID " + newAccountId +
+                                                " but different Reference exists: " + net["network"]
+                                }
+                            )
+
+                    #  check the regions/network count limits.
+                    totalNetworks = newAccountIdNetworks
+                    totalNetworks.extend(oldAccountIdNetworks)
+
+                    # CLOUD_MAX_ACCOUNT_REGION is the maximum number of regions for Account ID.
+                    if hasattr(settings, "CLOUD_MAX_ACCOUNT_REGION"):
+                        if settings.CLOUD_MAX_ACCOUNT_REGION < len(set([net.get("extattrs", {}).get("City", {}).get("value", "") for net in totalNetworks ])):
+                            raise CustomException(
+                                status=400,
+                                payload={
+                                    "Infoblox": "Cannot change the account: others networks exists for this account and the maximum number of regions for account should not be exceed: " + str(
+                                        settings.CLOUD_MAX_ACCOUNT_REGION)
+                                }
+                            )
+
+                    # CLOUD_MAX_ACCOUNT_REGION_NETS is the maximum number of networks for Account ID in a region.
+                    if hasattr(settings, "CLOUD_MAX_ACCOUNT_REGION_NETS"):
+                        regionsNetworks = [net.get("extattrs", {}).get("City", {}).get("value", "") for net in totalNetworks]
+                        for region in set(regionsNetworks):
+                            if settings.CLOUD_MAX_ACCOUNT_REGION_NETS < regionsNetworks.count(region):
+                                raise CustomException(
+                                    status=400,
+                                    payload={
+                                        "Infoblox": "Cannot change the account: others networks exists for this account and the maximum number of networks for region for account should not be exceed: " + str(
+                                            settings.CLOUD_MAX_ACCOUNT_REGION_NETS)}
+                                )
+
+            # If the Account Name change, check if it's already used for another Account ID.
+            if newAccountName != [net.get("extattrs", {}).get("Account Name", {}).get("value", "") for net in self.oldAccountIdNetworks][0]:
+                newAccountNameNetworks = self.__getAccountCloudNetworks(accountName=newAccountName)
+                if newAccountNameNetworks:
+                    for net in newAccountNameNetworks:
+                        if net.get("extattrs", {}).get("Account ID", {}).get("value", "") != newAccountId:
+                            raise CustomException(
+                                status=400,
+                                payload={
+                                    "Infoblox": "A network with the Account Name " + newAccountName +
+                                                " but different Account ID exists: " + net["network"]
+                                }
                             )
 
         except Exception as e:
