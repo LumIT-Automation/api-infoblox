e4b5a5154e0f2612c8da9186f1e333e823b96b25
mik <m.sartori@lumit.it>
2024-06-24 15:32:08 +0200

    workflow permissions

--- infoblox/controllers/Infoblox/Vlans.py
+++ infoblox/controllers/Infoblox/Vlans.py
@@ -21,57 +21,62 @@ class InfobloxVlansController(CustomController):
         itemData = dict()
         etagCondition = { "responseEtag": "" }
         user = CustomController.loggedUser(request)
+        workflowId = request.headers.get("workflowId", "") # a correlation id.
+        checkWorkflowPermission = request.headers.get("checkWorkflowPermission", "")
 
         fk = list()
         fv = list()
         filters = dict()
 
         try:
-            if Permission.hasUserPermission(groups=user["groups"], action="vlans_get", assetId=assetId) or user["authDisabled"]:
-                Log.actionLog("Get vlans list", user)
-
-                if 'fby' in request.GET and 'fval' in request.GET:
-                    for f in dict(request.GET)["fby"]:
-                        fk.append(f)
-                    for v in dict(request.GET)["fval"]:
-                        fv.append(v)
-                    filters = dict(zip(fk, fv))
-
-                lock = Lock("vlan", locals())
-                if lock.isUnlocked():
-                    lock.lock()
-
-                    itemData["data"] = Vlan.listData(assetId, filters)
-                    serializer = Serializer(data=itemData)
-                    if serializer.is_valid():
-                        data["data"] = serializer.validated_data["data"]
-                        data["href"] = request.get_full_path()
-
-                        # Check the response's ETag validity (against client request).
-                        conditional = Conditional(request)
-                        etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
-                        if etagCondition["state"] == "fresh":
-                            data = None
-                            httpStatus = status.HTTP_304_NOT_MODIFIED
-                        else:
-                            httpStatus = status.HTTP_200_OK
-                    else:
-                        httpStatus = status.HTTP_500_INTERNAL_SERVER_ERROR
-                        data = {
-                            "Infoblox": {
-                                "error": "Infoblox upstream data mismatch."
-                            }
-                        }
-
-                        Log.log("Upstream data incorrect: "+str(serializer.errors))
-
-                    lock.release()
-
-                    # Run registered plugins.
-                    CustomController.plugins("vlans_get")
+            if Permission.hasUserPermission(groups=user["groups"], action="vlans_get", assetId=assetId, isWorkflow=bool(workflowId)) or user["authDisabled"]:
+                if workflowId and checkWorkflowPermission:
+                    httpStatus = status.HTTP_204_NO_CONTENT
                 else:
-                    data = None
-                    httpStatus = status.HTTP_423_LOCKED
+                    Log.actionLog("Get vlans list", user)
+
+                    if 'fby' in request.GET and 'fval' in request.GET:
+                        for f in dict(request.GET)["fby"]:
+                            fk.append(f)
+                        for v in dict(request.GET)["fval"]:
+                            fv.append(v)
+                        filters = dict(zip(fk, fv))
+
+                    lock = Lock("vlan", locals())
+                    if lock.isUnlocked():
+                        lock.lock()
+
+                        itemData["data"] = Vlan.listData(assetId, filters)
+                        serializer = Serializer(data=itemData)
+                        if serializer.is_valid():
+                            data["data"] = serializer.validated_data["data"]
+                            data["href"] = request.get_full_path()
+
+                            # Check the response's ETag validity (against client request).
+                            conditional = Conditional(request)
+                            etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
+                            if etagCondition["state"] == "fresh":
+                                data = None
+                                httpStatus = status.HTTP_304_NOT_MODIFIED
+                            else:
+                                httpStatus = status.HTTP_200_OK
+                        else:
+                            httpStatus = status.HTTP_500_INTERNAL_SERVER_ERROR
+                            data = {
+                                "Infoblox": {
+                                    "error": "Infoblox upstream data mismatch."
+                                }
+                            }
+
+                            Log.log("Upstream data incorrect: "+str(serializer.errors))
+
+                        lock.release()
+
+                        # Run registered plugins.
+                        CustomController.plugins("vlans_get")
+                    else:
+                        data = None
+                        httpStatus = status.HTTP_423_LOCKED
             else:
                 data = None
                 httpStatus = status.HTTP_403_FORBIDDEN
