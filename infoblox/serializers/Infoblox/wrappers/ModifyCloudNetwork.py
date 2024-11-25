4a63c4e9b1ae9c989f361efdac3e90c99b181aee
mik <m.sartori@lumit.it>
2024-11-25 11:25:35 +0100

    ModifyCloudNetwork fix

--- infoblox/serializers/Infoblox/wrappers/ModifyCloudNetwork.py
+++ infoblox/serializers/Infoblox/wrappers/ModifyCloudNetwork.py
@@ -2,11 +2,6 @@ from rest_framework import serializers
 
 class InfobloxModifyCloudNetworkSerializer(serializers.Serializer):
     class InfobloxModifyNetworkDataSerializer(serializers.Serializer):
-        class InfobloxNetworkValueStringSerializer(serializers.RegexField):
-            def __init__(self, *args, **kwargs):
-                regex = '^([01]?\d\d?|2[0-4]\d|25[0-5])(?:\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])){3}(?:/[0-2]\d|/3[0-2])?$|^next-available$'
-                super().__init__(regex=regex, *args, **kwargs)
-
         class InfobloxNetworkInnerExtattrsSerializer(serializers.Serializer):
             def __init__(self, *args, **kwargs):
                 super().__init__(*args, **kwargs)
@@ -31,7 +26,4 @@ class InfobloxModifyCloudNetworkSerializer(serializers.Serializer):
         extattrs = InfobloxNetworkInnerExtattrsSerializer(required=False)
         comment = serializers.CharField(max_length=255, allow_blank=True, required=False)
 
-
-    #provider = serializers.CharField(max_length=255, required=False)
-    #region = serializers.CharField(max_length=255, required=False, allow_blank=True)
     network_data = InfobloxModifyNetworkDataSerializer(required=True)
